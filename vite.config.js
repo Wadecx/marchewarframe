441b63abcff70f1e23c15aa5072215e29b2c56e3
Wadecx <tyronmorcel@gmail.com>
2025-01-04 06:53:38 +0100

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': { // Toutes les requêtes commençant par /api seront proxyfiées
+        target: 'https://api.warframe.market', // L'URL de votre API
+        changeOrigin: true, // Change l'origine pour éviter les problèmes CORS
+        rewrite: (path) => path.replace(/^\/api/, ''), // Enlève le préfixe /api dans l'URL
+      },
+    },
+  },
 });
